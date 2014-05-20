43b5342721f152592b1ebe5bdeafcea3aafc7e2b
Zollar <zollar@ubuntu.(none)>
2014-05-20 10:12:26 -0400

    added a close button to submission history. Changed format for datestamp

--- pcrs/problems_code/static/problems_code/js/problem.js
+++ pcrs/problems_code/static/problems_code/js/problem.js
@@ -4,6 +4,7 @@
 */
 
 var testcases = null;
+var code_problem_id = -1;
 
 function changeView(mode) {
 
@@ -71,6 +72,7 @@ function prepareGradingTable() {
 
     var gradingTable = $("#gradeMatrix");
     var score = 0;
+    console.log(testcases[0]);
 
     for (var i = 0; i < testcases.length; i++) {
         var current_testcase = testcases[i];
@@ -120,15 +122,35 @@ function prepareGradingTable() {
     add_to_history(score);
 }
 
+function create_timestamp(datetime){
+    month_names = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    day = datetime.getDate();
+    month = month_names[datetime.getMonth()];
+    year = datetime.getYear();
+    hour = datetime.getHours();
+    minute = datetime.getMinutes();
+    if (hour > 12){
+        hour -= 12;
+        cycle = "p.m.";
+    }
+    else{
+        cycle = "a.m.";
+    }
+
+    formated_datetime = month + " " + day + ","+year + ", " + hour+":"+minute+" "+cycle
+    return formated_datetime;
+}
+
 function add_to_history(score){
     var sub_history_window = $('#accordion');
     var datetime = new Date();
+    datetime = create_timestamp(datetime);
 
     var new_entry = '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title">'
-    new_entry += '<a data-toggle="collapse" data-parent="#accordion" href="#collapse_999" onclick="delay_refresh_cm()">'
+    new_entry += '<a data-toggle="collapse" data-parent="#accordion" href="#collapse_' + code_problem_id + '" onclick="delay_refresh_cm()">'
     new_entry += datetime + '<td> Score :' + score +'/'+ testcases.length + '</td></a></h4></div>'
-    new_entry += '<div id="collapse_999" class="panel-collapse collapse">'
-    new_entry += '<div id="history_mirror_999_999">' + myCodeMirror.getValue() + '</div>'
+    new_entry += '<div id="collapse_'+ code_problem_id + '" class="panel-collapse collapse">'
+    new_entry += '<div id="history_mirror_999_'+code_problem_id+'">' + myCodeMirror.getValue() + '</div>'
     new_entry += '<ul class="list-group">'
     for (var i = 0; i < testcases.length; i++) {
         var test_case = testcases[i];
@@ -156,7 +178,8 @@ function add_to_history(score){
     new_entry += '</ul></div></div>';
 
     sub_history_window.prepend(new_entry);
-    create_history_code_mirror ('python', 3, 'history_mirror_999_999');
+    create_history_code_mirror ('python', 3, 'history_mirror_999_'+code_problem_id);
+    code_problem_id -= 1;
 }
 
 $( document).ready(function() {
