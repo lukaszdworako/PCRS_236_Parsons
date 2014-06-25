911be99369539be672bfbff60fc2c9adcc61e414
Zollar <zollar@ubuntu.(none)>
2014-06-25 15:37:22 -0400

    on Content/challenges/#/objects problems on the left side now have tags, the search also looks at tags

--- pcrs/content/static/content/js/challenge_objects.js
+++ pcrs/content/static/content/js/challenge_objects.js
@@ -175,14 +175,23 @@ function find_problems(){
     var searching_for = $('#searcher').val().toLowerCase()
     var problem_list = $('.tab-pane.active').children().first().children();
     for (var index = 0; index < problem_list.length; index ++){
-                if (searching_for == ""){
+        if (searching_for == ""){
             $(problem_list[index]).show();
         }
-        if ($(problem_list[index]).find('b').text().toLowerCase().indexOf(searching_for) != -1){
+        else if ($(problem_list[index]).find('b').text().toLowerCase().indexOf(searching_for) != -1){
             $(problem_list[index]).show();
         }
         else{
             $(problem_list[index]).hide();
         }
+        var current_tags = $(problem_list[index]).find('.badge.tag');
+        if (searching_for != ""){
+            for (var tag_index = 0; tag_index < current_tags.length; tag_index++){
+                if ($(current_tags[tag_index]).text().toLowerCase().indexOf(searching_for) != -1){
+                    $(problem_list[index]).show();
+                }
+            }
+        }
+
     }
 }
