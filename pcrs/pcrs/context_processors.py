5126c9a3a14017cab1564ff5068cc45c32e63ec5
Andrew Petersen <andrew.petersen@utoronto.ca>
2014-07-21 10:59:35 -0400

    Adding installed problems flags for templates.

--- pcrs/pcrs/context_processors.py
+++ pcrs/pcrs/context_processors.py
@@ -2,4 +2,5 @@ from django.conf import settings
 
 def site_settings(request):
     languages = [language[0] for language in settings.LANGUAGE_CHOICES]
+    problem_types = [pt for pt in settings.INSTALLED_PROBLEM_APPS]
     return {'site_prefix': settings.SITE_PREFIX, 'languages': languages}
