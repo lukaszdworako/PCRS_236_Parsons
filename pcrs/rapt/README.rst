66fae292c13258b20a06db4493c860e3cef3f676
Olessia Karpova <olessia.karpova@mail.utoronto.ca>
2014-08-11 16:29:44 -0400

    Update to next version of rapt. Removed rapt folder from pcrs. Unpack the tar in resources, and install the package within your virtual environment. Do not recommit the package itself.

--- pcrs/rapt/README.rst
+++
@@ -1,6 +0,0 @@
-rapt
-====
-
-The Relational Algebra Parser and Translator.
-
-rapt is a relational algebra parser and translator.
--- pcrs/rapt/rapt/tests/setup_db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE rapt_test_db;
-CREATE USER raptor WITH PASSWORD 'raptolicious';
-GRANT ALL ON DATABASE rapt_test_db TO raptor;
