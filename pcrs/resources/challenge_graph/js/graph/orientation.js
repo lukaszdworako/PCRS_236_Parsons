5548167d560d0ffc70fad2c44d9c823ebff83c23
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-08-11 19:51:53 -0400

    Graph: Map dimensions improved.
    The map dimensions are a bit hacky, but for now they work.

--- pcrs/resources/challenge_graph/js/graph/orientation.js
+++ pcrs/resources/challenge_graph/js/graph/orientation.js
@@ -24,28 +24,25 @@ function setChangeOrientationEvent() {
                 .attr("points", $(this).find("polygon").attr("points"));
         });
         d3.selectAll(".edge").transition().duration(5000).style("opacity", 1);
-//        d3.select("svg").attr("viewbox", viewBox);
         $("svg:first").attr("width", $(graph).attr("width"));
         $("svg:first").attr("height", $(graph).attr("height"));
         $("#nav-graph").attr("width", parseFloat($(graph).attr("width")) * 0.1);
         $("#nav-graph").attr("height", parseFloat($(graph).attr("height")) * 0.1);
-
+        console.log(parseFloat($(graph).attr("height")));
         $("#nav-graph").find("#graph0").attr("transform", $(graph).find("#graph0").attr("transform"));
-        $("#graph0").attr("transform", $(graph).find("#graph0").attr("transform"));
+        d3.select("#graph0").transition().duration(5000).attr("transform", $(graph).find("#graph0").attr("transform"));
+
         $.each($(graph).find(".node"), function (i, node) {
-            console.log($(this).find('rect').attr("x"));
             d3.select("#graph").select("#" + $(this).attr("id"))
                 .select("rect").transition().duration(5000).attr({x: $(node).find('rect').attr("x"),
             y: $(node).find('rect').attr("y")});
             var texts = $("#graph")
                 .find("#" + $(this)
                     .attr("id")).find("text");
-            console.log("Length : " + texts.length);
             var graphNode = $("#" + $(node).attr("id"));
             $(texts).each(function (j) {
                 var text = $(node).find('text').get(j);
                 var graphText = $(graphNode).find('text').get(j);
-                console.log("hey #" + $(graphText).attr("id"));
                 d3.select("#" + $(node)
                     .attr("id"))
                     .select("#" + $(graphText).attr("id"))
@@ -55,7 +52,6 @@ function setChangeOrientationEvent() {
                     y: $(text).attr("y")});
             })
         });
-
         $.each($(graph).find(".node"), function (i, node) {
             d3.select("#nav-graph").select("#" + $(this).attr("id") + "-map-node")
                 .select("rect").transition().duration(5000).attr({x: $(node).find('rect').attr("x"),
