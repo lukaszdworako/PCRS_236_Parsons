af35b307eda9b385fd856bf7b8e60e4a8e3e6ced
Andrew Petersen <andrew.petersen@utoronto.ca>
2014-08-14 02:31:58 -0400

    Improvement on code to ignore uninstalled modules. No longer includes the files.

--- pcrs/problems/urls.py
+++ pcrs/problems/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from pcrs.settings import INSTALLED_PROBLEM_APPS
 
-pattern_options = {'problems_code': url(r'^code/', include('problems_code.urls')),
-    'problems_multiple_choice': url(r'^multiple_choice/', include('problems_multiple_choice.urls')),
-    'problems_rdb': url(r'^rdb/', include('problems_rdb.urls')),
-    'problems_sql': url(r'^sql/', include('problems_sql.urls')),
-    'problems_ra': url(r'^ra/', include('problems_ra.urls')),
+pattern_options = {'problems_code': (r'^code/', 'problems_code.urls'),
+    'problems_multiple_choice': (r'^multiple_choice/', 'problems_multiple_choice.urls'),
+    'problems_rdb': (r'^rdb/', 'problems_rdb.urls'),
+    'problems_sql': (r'^sql/', 'problems_sql.urls'),
+    'problems_ra': (r'^ra/', 'problems_ra.urls'),
 }
 
-pattern_list = [''] + [pattern_options[mod] for mod in INSTALLED_PROBLEM_APPS]
+pattern_list = [''] + [url(pattern_options[mod][0], include(pattern_options[mod][1])) for mod in INSTALLED_PROBLEM_APPS]
 
 urlpatterns = patterns(*pattern_list)
 
