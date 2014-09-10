8a32e8a259530513064cc61bd55ae932cdc5c85a
Andrew Petersen <andrew.petersen@utoronto.ca>
2014-09-09 22:13:20 -0400

    Updated U of T management scripts. Added a favicon because the error in the log bugged the heck out of me.

--- admin_scripts/add_pcrs_users.py
+++ admin_scripts/add_pcrs_users.py
@@ -4,34 +4,32 @@ import csv
 # Usage:
 #   add <current_classlist> <past_classlist> 
 # Globals:
-dbname = "StG108"
-section = "123"
+dbname = "UTM108"
 
 past_users = []
+user_section = {}
 with open(sys.argv[2]) as csvfile:
     past_cl = csv.reader(csvfile, delimiter=",")
     for row in past_cl:
         past_users.append(row[0])
+        user_section[row[0]] = row[3]
 
-new_users = []
+sqlfile = open(dbname + ".sql", "w")
 with open(sys.argv[1]) as csvfile:
     curr_cl = csv.reader(csvfile, delimiter=",")
     for row in curr_cl:
+        if "0102" in row[3]:    # Online
+            section = "123"
+        else:
+            section = "Lecture"
+
         if row[0] not in past_users:
-            new_users.append(row[0])
+            sqlfile.write("insert into users_pcrsuser (last_login, username, section_id, code_style, is_student, is_ta, is_instructor, is_active, is_admin, is_staff) values (CURRENT_TIMESTAMP, '%s', '%s', 0, True, False, False, True, False, False);\n" % (row[0], section))
         else:
             past_users.remove(row[0])
+            if row[3] != user_section[row[0]]:
+                sqlfile.write("UPDATE users_pcrsuser SET section_id = '%s' where username = '%s';\n" % (section, row[0]))
 
-# Any new_users need accounts.
 # Any past_users have dropped and should become inactive.
-
-with open(dbname + ".sql", "w") as sqlfile:
-
-    sqlfile.write("COPY users_pcrsuser (last_login, username, section_id, code_style, is_student, is_ta, is_instructor, is_active, is_admin, is_staff) from STDIN;\n")
-    for user in new_users:
-        sqlfile.write("2013-09-10 12:00:00.000000-04\t%s\t%s\t0\tTrue\tFalse\tFalse\tTrue\tFalse\tFalse\n" % (user, section))
-    sqlfile.write("\\.\n")
-
-    for user in past_users:
-        sqlfile.write("UPDATE users_pcrsuser SET is_active = False where username = '%s';\n" % (user))
-
+for user in past_users:
+    sqlfile.write("UPDATE users_pcrsuser SET is_active = False where username = '%s';\n" % (user))
