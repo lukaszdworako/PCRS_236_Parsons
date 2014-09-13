b72e28d851e3d064eba927765b6610297d3f2813
Olessia Karpova <olessia.karpova@mail.utoronto.ca>
2014-09-13 12:19:31 -0400

    ui fix

--- pcrs/content/static/content/js/socket_events.js
+++ pcrs/content/static/content/js/socket_events.js
@@ -9,20 +9,3 @@ socket.on(userhash, function (data) {
         new CustomEvent('statusUpdate', {detail: data})
     );
 });
-
-
-// Listen for updates.
-// These events are dispatched by the socket listener.
-window.addEventListener('statusUpdate', function (event) {
-    var receiver = 'statusUpdate' + event.detail.id;
-    window.dispatchEvent(
-        new CustomEvent(receiver, {detail: event.detail})
-    );
-}, false);
-
-window.addEventListener('itemUpdate', function (event) {
-    var receiver = 'itemUpdate' + event.detail.item.id;
-    window.dispatchEvent(
-        new CustomEvent(receiver, {detail: event.detail})
-    );
-}, false);
