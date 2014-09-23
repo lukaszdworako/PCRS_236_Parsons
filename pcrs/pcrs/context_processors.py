41a1908aa20357caa2af758f8465b7cffb61bfd6
Andrew Petersen <andrew.petersen@utoronto.ca>
2014-09-23 14:55:48 -0400

    Adding name of logged in users to navbar (for students). Removing logout for shibboleth.

--- pcrs/pcrs/context_processors.py
+++ pcrs/pcrs/context_processors.py
@@ -4,4 +4,5 @@ def site_settings(request):
     languages = [language[0] for language in settings.LANGUAGE_CHOICES]
     problem_types = [pt for pt in settings.INSTALLED_PROBLEM_APPS]
     return {'site_prefix': settings.SITE_PREFIX, 'languages': languages, 
-            'problem_types': problem_types, 'mymedia_videos': settings.MYMEDIA_VIDEOS}
+            'problem_types': problem_types, 'mymedia_videos': settings.MYMEDIA_VIDEOS, 
+            'auth_shibboleth': settings.AUTH_TYPE == 'shibboleth'}
