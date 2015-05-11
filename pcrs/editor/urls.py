5c3f3d8bbed06b73ac883f771feaaa8dd04c41f9
jpaprakis <j.paprakis@mail.utoronto.ca>
2015-05-11 11:46:52 -0400

    Got editor urls and views working, now need to just work on the front-end to get it to display properly

--- pcrs/editor/urls.py
+++ pcrs/editor/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from editor.views import EditorView
+import problems_c.models as c_models
+import problems_python.models as python_models
 
 urlpatterns = patterns('',
     url(r'^c$',
-        TemplateView.as_view(template_name='editor/c_editor.html')),
+        EditorView.as_view(model=c_models.Submission, template_name='editor/c_editor.html', pType='c')),
 
     url(r'^python$',
-        TemplateView.as_view(template_name='editor/python_editor.html')),
+        EditorView.as_view(model=python_models.Submission, template_name='editor/python_editor.html', pType='python')),
 )
--- pcrs/editor/views.py
+++ pcrs/editor/views.py
@@ -4,12 +4,76 @@ import logging
 
 from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404, render
-from django.views.generic import (DetailView, UpdateView, DeleteView, FormView,
-                                  View)
+from django.views.generic import (DetailView, UpdateView, DeleteView, FormView, View)
 from django.views.generic.detail import SingleObjectMixin
 from django.utils.timezone import localtime
-
+from problems.forms import ProgrammingSubmissionForm
 from pcrs.generic_views import (GenericItemCreateView, GenericItemListView,
                                 GenericItemUpdateView)
+from users.views import UserViewMixin
+from users.views_mixins import ProtectedViewMixin
+import problems_c.models as c_models
+import problems_python.models as python_models
 
-class EditorView():
+class EditorViewMixin:
+    def get_section(self):
+        return None
+
+    def get_problem(self):
+        """
+        Return the Problem object for the submission.
+        """
+        logging.info('page_processor logging test')
+        if(self.pType == 'c'):
+        	p = c_models.Problem(name="blank", starter_code="")
+        elif(self.pType == 'python'):
+       		p = python_models.Problem(name="blank", starter_code="")
+       	else:
+       		logging.info('NONE PTYPE')
+       		p = None
+        return p
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['problem'] = self.get_problem()
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        return None
+
+    def record_submission(self, request):
+        """
+        Record the submission and return the results of running the testcases.
+        """
+        submission_model = self.model.get_submission_class()
+        submission_code = request.POST.get('submission', '')
+        results, error = [], None
+        if submission_code:
+            submission = submission_model.objects.create(
+                user=request.user, problem=self.get_problem(),
+                section=self.get_section(), submission=submission_code)
+            results, error = submission.run_testcases(request)
+            submission.set_score()
+            self.object = submission
+        return results, error
+
+    def post(self, request, *args, **kwargs):
+        """
+        Record the submission and redisplay the problem submission page,
+        with latest submission prefilled.
+        """
+        form = self.get_form(self.get_form_class())
+        results, error = self.record_submission(request)
+        return self.render_to_response(
+            self.get_context_data(form=form, results=results, error=error,
+                                  submission=self.object))
+
+
+class EditorView(ProtectedViewMixin, EditorViewMixin, SingleObjectMixin,
+                     FormView, UserViewMixin):
+    """
+    Create a submission for a problem.
+    """
+    pType = None
+    form_class = ProgrammingSubmissionForm
+    object = None
