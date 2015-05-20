833e336200674b852ca8e45bcf40e5fe1ab6445c
jpaprakis <jpaprakis@gmail.com>
2015-05-20 18:32:25 +0000

    simple.c created online with Bitbucket

---
+++ pcrs/languages/c/visualizer/simple.c
@@ -0,0 +1,21 @@
+*/
+A silly c program I made to test out the parser
+/*
+include <stdio.h>
+int main(int argc, char **argv)
+{
+	int i;
+	i = 3;
+	int *array;
+	int j;
+	char *myptr;
+	myptr = malloc(2);
+	myptr = 'j';
+	j = 2;
+	array = j;
+	printf("hello world%d", 2);
+	printf("hey");
+	int q;
+	q = 4;
+	return 0;
+}
