cb88e9286df4e8950dff7e7a7570adcad3545f48
jpaprakis <j.paprakis@mail.utoronto.ca>
2015-06-08 14:06:25 -0400

    got front end working with std err and showing that text in red

--- pcrs/languages/c/visualizer/pycparser-experimentation/sample_c_files/stdout-test.c
+++ pcrs/languages/c/visualizer/pycparser-experimentation/sample_c_files/stdout-test.c
@@ -7,7 +7,9 @@ int main() {
 	printf("Oops I forgot a new line above!!!\n");
 	char x[1];
 	x[0] = 'a';
+	fprintf(stderr, "error!!!");
 	printf("my char array has %c in it\n", x[0]);
 	printf("bye!\n");
+	fprintf(stderr, "some more errors\n");
 	return 0;
 }
