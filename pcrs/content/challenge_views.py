a53d2521899d13ac16e232dac069eff58ba5d1af
Andrew Petersen <andrew.petersen@utoronto.ca>
2015-07-02 13:50:32 -0400

    Removing error when a None object is prefetched

--- pcrs/content/challenge_views.py
+++ pcrs/content/challenge_views.py
@@ -171,7 +171,7 @@ class ReactiveContentPageData(ContentPageView, View):
         items = [
             item.content_object.serialize()
             for item in ContentSequenceItem.objects.filter(content_page=page)
-                        .prefetch_related('content_object').all()
+                        .prefetch_related('content_object').all() if item
         ]
 
         scores = {}
