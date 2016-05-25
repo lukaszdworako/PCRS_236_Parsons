c4ad6e355dffa7503f08f79f3cbf36927cc02540
Elijah Mirecki <elimirks@gmail.com>
2016-05-25 16:49:25 -0400

    Changed the clear submissions page into a dialog.

--- pcrs/problems_java/forms.py
+++ pcrs/problems_java/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from crispy_forms.layout import HTML
 
 from pcrs.form_mixins import BaseRelatedObjectForm
 from problems.forms import BaseProblemForm
@@ -6,6 +7,11 @@ from .models import Problem
 
 
 class ProblemForm(forms.ModelForm, BaseProblemForm):
+    # Overridden from BaseProblemForm
+    clear_button = HTML('<a class="red-button" role="button" '
+        'onclick="showClearSubmissionsDialog()"/clear">'
+        'Clear submissions</a>')
+
     class Meta:
         model = Problem
         fields = ('name', 'description', 'starter_code', 'solution',
