c97426c0e9f6016ecd58cc53abc7c960667c6232
Andrew Petersen <andrew.petersen@utoronto.ca>
2016-06-24 14:49:20 -0400

    Adding JAVA_HOME variable to shell script

--- pcrs/languages/java/execution/run_java_jail.sh
+++ pcrs/languages/java/execution/run_java_jail.sh
@@ -3,6 +3,9 @@
 
 cd JavaJail
 
+: ${JAVA_HOME="./java"}
+export JAVA_HOME
+
 if [ ! -e ./build ]
 then
     echo You must pull and configure the JavaJail git submodule.
@@ -11,5 +14,5 @@ fi
 
 export CLASSPATH="./build/:./jar/javax.json-1.0.jar:$JAVA_HOME/lib/tools.jar"
 # Forward STDIN directly into the jail
-cat | java traceprinter.InMemory
+cat | $JAVA_HOME/bin/java traceprinter.InMemory
 
