8c421727585a8a821e7c149c3bff3d13862ce625
Elijah Mirecki <elimirks@gmail.com>
2016-07-22 20:33:20 -0400

    Sorting is done!
    We just need to plop a link to this guy somewhere now

--- pcrs/content/static/content/js/quest_analytics.js
+++ pcrs/content/static/content/js/quest_analytics.js
@@ -1,15 +1,25 @@
-function QuestAnalyticsRenderer(userCount, problems) {
+/**
+ * @param {number} userCount The amount of users in this quest
+ * @param {array} problems A list of problem analytics
+ * @param {string} problems[].name
+ * @param {string} problems[].language
+ * @param {number} problems[].medianAttempts
+ * @param {number} problems[].hasAttemptedCount
+ * @param {number} problems[].hasSolvedCount
+ */
+function QuestAnalyticsRenderer(userCount, problems, $analyticsTable) {
     this.userCount = userCount;
     this.problems = problems;
+    this.$analyticsTable = $analyticsTable;
 }
 
-QuestAnalyticsRenderer.prototype.renderTo = function($analyticsTable) {
-    // No sorting initially
-    this._renderProblemsToTable(this.problems, $analyticsTable);
+QuestAnalyticsRenderer.prototype.render = function() {
+    this._renderProblems(this.problems);
+    this.$analyticsTable.tablesort(); // jQuery plugin
 }
 
-QuestAnalyticsRenderer.prototype._renderProblemsToTable =
-        function(problems, $table) {
+QuestAnalyticsRenderer.prototype._renderProblems = function(problems) {
+    this.$analyticsTable.find('.analytics-problem-row').remove();
     for (var i = 0; i < problems.length; i++) {
         var problem = problems[i];
 
@@ -17,11 +27,9 @@ QuestAnalyticsRenderer.prototype._renderProblemsToTable =
             .append($('<td></td>').text(problem.name))
             .append($('<td></td>').text(problem.language))
             .append($('<td></td>').text(problem.medianAttempts))
-            .append($('<td></td>').text(
-                problem.hasAttemptedCount + ' / ' + this.userCount))
-            .append($('<td></td>').text(
-                problem.hasSolvedCount + ' / ' + this.userCount))
-        $table.append($row);
+            .append($('<td></td>').text(problem.hasAttemptedCount))
+            .append($('<td></td>').text(problem.hasSolvedCount))
+        this.$analyticsTable.find('tbody').append($row);
     }
 }
 
