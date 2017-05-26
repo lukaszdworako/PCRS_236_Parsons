2f25527137d0230eabb1fc29766d27abebe28c9e
Jeremy Chinsen <jeremy@Jeremys-MacBook-Air.local>
2017-05-26 11:46:42 -0400

    Added embed functionality in templates and urls. Also implemneted side-by-side graph comparison in problem submission

--- pcrs/problems_r/r_language.py
+++ pcrs/problems_r/r_language.py
@@ -422,6 +422,7 @@ class RSpecifics(languages.BaseLanguage):
 			ret.pop("test_val", None)
 >>>>>>> added models
 			ret["exception"] = str(e)
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -445,5 +446,7 @@ class RSpecifics(languages.BaseLanguage):
 		return ret
 >>>>>>> Just execute entire script and redirect output to temporary file
 =======
+=======
+>>>>>>> Added embed functionality in templates and urls. Also implemneted side-by-side graph comparison in problem submission
 		return ret
 >>>>>>> Installation instructions
