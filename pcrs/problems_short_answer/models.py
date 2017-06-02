9c6e02812bb209c9ba2b7c796bfedc2a4e862773
Jeremy Chinsen <jeremy@Jeremys-MacBook-Air.local>
2017-06-02 16:41:15 -0400

    Changed the default max_chars of short answers.

--- pcrs/problems_short_answer/models.py
+++ pcrs/problems_short_answer/models.py
@@ -14,7 +14,7 @@ class Problem(AbstractProblem):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     solution = models.TextField(help_text='The solution should be valid Python code. It may import libraries, such as <i>re</i>. The submission to be graded will be in a variable named <i>submission</i>, and the score to be assigned should be placed in a variable <i>score</i>. An optional error message to be displayed to the student can be placed in variable <i>message</i>.')
-    max_chars = models.PositiveIntegerField(default=0)
+    max_chars = models.PositiveIntegerField(default=200)
 
     def clean_fields(self, exclude=None):
         super().clean_fields(exclude)
