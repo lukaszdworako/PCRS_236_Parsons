69d3d514b8f141dfc2351d8a6d0ded741673bcc1
Jeremy Chinsen <jeremy@Jeremys-MacBook-Air.local>
2017-06-05 13:55:54 -0400

    Implemented output visibility field for R problems

--- pcrs/problems_r/r_language.py
+++ pcrs/problems_r/r_language.py
@@ -119,9 +119,13 @@ class RSpecifics(languages.BaseLanguage):
 			ret["passed_test"] = (ret["test_val"] == solution["test_val"])
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 			ret["sol_val"] = solution["test_val"]
 =======
 >>>>>>> Reverted changes to forms and added a default seed unique to current user
+=======
+			ret["sol_val"] = solution["test_val"]
+>>>>>>> Implemented output visibility field for R problems
 =======
 			ret["sol_val"] = solution["test_val"]
 >>>>>>> Implemented output visibility field for R problems
