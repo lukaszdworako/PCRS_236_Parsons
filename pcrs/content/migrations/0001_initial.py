8db6b5a09b8c936aaccc1d62f944444a7db1820f
Andrew Petersen <andrew.petersen@utoronto.ca>
2017-07-04 22:14:11 +0200

    Patching migrations for content: included items that were not in the database before summer 2017

--- pcrs/content/migrations/0001_initial.py
+++ pcrs/content/migrations/0001_initial.py
@@ -87,18 +87,6 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='SectionQuestPosition',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('x', models.IntegerField()),
-                ('y', models.IntegerField()),
-                ('content_sectionquest', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='content.SectionQuest')),
-            ],
-            options={
-                'db_table': 'content_sectionquest_positions',
-            },
-        ),
-        migrations.CreateModel(
             name='Tag',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
