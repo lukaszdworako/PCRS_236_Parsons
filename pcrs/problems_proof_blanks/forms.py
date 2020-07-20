b00c8bc298e4b83e37b2a472d793e83cf4e30cc9
Naaz <sibianjali@gmail.com>
2020-07-19 22:53:30 -0400

    Delete "add more feedback" button from Feedback Form

--- pcrs/problems_proof_blanks/forms.py
+++ pcrs/problems_proof_blanks/forms.py
@@ -2,7 +2,7 @@ from crispy_forms.layout import HTML, ButtonHolder, Submit, Layout, Fieldset, Di
 from django import forms
 from django.contrib.postgres.forms.jsonb import JSONField
 
-from pcrs.form_mixins import CrispyFormMixin, BaseRelatedObjectForm
+from pcrs.form_mixins import CrispyFormMixin
 from problems.forms import BaseProblemForm, BaseSubmissionForm
 from problems_proof_blanks.models import Problem, Feedback, Submission
 
@@ -27,14 +27,60 @@ class ProblemForm(forms.ModelForm, BaseProblemForm):
 
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
-        self.save_and_add = Submit('submit', 'Next',
-                               css_class='green-button-right',
-                               formaction='create_next')
+        self.save_and_add_button = Submit('submit', 'Save and add testcases',
+            css_class='green-button-right',
+            formaction='create_and_add_testcase')
         BaseProblemForm.__init__(self)
+    
+
+    def _createButtons(self):
+        '''Generate the buttons for the problem form.
+
+        Returns:
+            A tuple of button elements
+        '''
+        absUrl = self.instance.get_absolute_url()
+        cloneUrl = '{}/clone'.format(absUrl)
+        clearUrl = '{}/clear'.format(absUrl)
+
+        self.clear_button = HTML('<a class="red-button" role="button" '
+            'onclick="showClearSubmissionsDialog(\'' + clearUrl + '\')">'
+            'Clear submissions</a>')
+        clone_button = Submit('clone', 'Clone',
+            css_class='green-button', formaction=cloneUrl)
+        save_button = Submit('submit', 'Save',
+            css_class='green-button-right')
+        attempt_button = Submit('attempt', 'Save & Attempt',
+            css_class='green-button-right')
+
+        # if existing feedback, redirect to edit page
+        if hasattr(self.instance, 'feedback'):
+            add_button = Submit('submit', 'Save and edit feedback',
+                css_class='green-button-right',
+                formaction='{0}/feedback/{0}'.format(self.instance.pk))
+        else:
+            add_button = Submit('submit', 'Save and add feedback',
+                css_class='green-button-right',
+                formaction='{}/feedback'.format(self.instance.pk))
+
+        # Existing problem
+        if self.instance.pk:
+            return (
+                Div(CrispyFormMixin.delete_button, self.clear_button,
+                    css_class='button-group'),
+                Div(clone_button, save_button, add_button, attempt_button,
+                    css_class='button-group-right'),
+            )
+        # Cloning a problem
+        elif self.instance:
+            return save_button,
+        # Creating a new problem
+        else:
+            return self.save_and_add_button,
 
 
 
-class FeedbackForm(BaseRelatedObjectForm):
+class FeedbackForm(CrispyFormMixin, forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ('feedback_keys', 'hint_keys', 'problem')
@@ -54,8 +100,18 @@ class FeedbackForm(BaseRelatedObjectForm):
         }
 
     def __init__(self, *args, **kwargs):
-        BaseRelatedObjectForm.__init__(self, *args, formaction='feedback',
-                                       **kwargs)
+        problem = kwargs.get('problem', None)
+        super().__init__(*args, **kwargs)
+        self.save_button = Submit('submit', 'Save',
+                                   css_class='green-button-right')
+        if self.instance.pk:
+            self.buttons = Div(self.delete_button, Div(self.save_button, 
+                                   css_class='button-group-right'))
+        else:
+            self.buttons = Div(Div(self.save_button, 
+                                   css_class='button-group-right'))
+        self.helper.layout = Layout(Fieldset('', *self.Meta.fields),
+                                    ButtonHolder(*self.buttons))
 
 
 class SubmissionForm(BaseSubmissionForm):
@@ -67,7 +123,6 @@ class SubmissionForm(BaseSubmissionForm):
         incomplete_proof = HTML('<p>{}</p> <br>'.format(problem.incomplete_proof))
         super().__init__(*args, **kwargs)
         fieldsets = []
-        print(problem.feedback_set.all())
         for question in problem.answer_keys:
             self.fields["submission_{}".format(question)] = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':1, 'size': '5'}), required=False, label=question, max_length=20)
             fieldsets.append(HTML('<span> <label> {}'.format(question)))
