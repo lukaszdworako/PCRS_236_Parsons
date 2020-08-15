b62dbb1669c368c737e703bfc40825fada8b0ef5
lukaszdworako <lukasz.dworakowski@utoronto.ca>
2020-08-15 13:44:44 -0400

    Fix exception in testcase rendering in frontend

--- pcrs/resources/parsons/parsons.js
+++ pcrs/resources/parsons/parsons.js
@@ -604,45 +604,45 @@
         .attr('style', 'width: 100%;')
         .append("<code>" + testcase.exception + "</code>"));
     }
+    else {
+      $newRow.append('<td class="description">' + testcase.test_desc + '</td>');
+      if (testcase.test_input != null) {
+        $newRow.append('<td class="expression"><div class="expression_div">' +
+          testcase.test_input + '</div></td>');
+      } else {
+        $newRow.append('<td class="expression">' +
+          "Hidden Test" + '</td>');
+      }
 
-    $newRow.append('<td class="description">' + testcase.test_desc + '</td>');
-    if (testcase.test_input != null) {
-      $newRow.append('<td class="expression"><div class="expression_div">' +
-        testcase.test_input + '</div></td>');
-    } else {
-      $newRow.append('<td class="expression">' +
-        "Hidden Test" + '</td>');
-    }
+      var expTestValDiv = $('<div class="ExecutionVisualizer"></div>');
+      var testResultDiv = $('<div class="ExecutionVisualizer"></div>');
 
-    var expTestValDiv = $('<div class="ExecutionVisualizer"></div>');
-    var testResultDiv = $('<div class="ExecutionVisualizer"></div>');
+      $newRow.append($('<td class="expected"></td>')
+        .append($('<div class="ptd"></div>')
+          .append(expTestValDiv)));
+      $newRow.append($('<td class="result"></td>')
+        .append($('<div class="ptd"></div>')
+          .append(testResultDiv)));
 
-    $newRow.append($('<td class="expected"></td>')
-      .append($('<div class="ptd"></div>')
-        .append(expTestValDiv)));
-    $newRow.append($('<td class="result"></td>')
-      .append($('<div class="ptd"></div>')
-        .append(testResultDiv)));
+      renderData_ignoreID(testcase.test_val, testResultDiv);
+      var expected;
+      if (testcase.expected_output)
+      {
+        expected = testcase.expected_output;
+      }
+      else
+      {
+        expected = "Hidden Result";
+      }
+      renderData_ignoreID(expected, expTestValDiv);
 
-    renderData_ignoreID(testcase.test_val, testResultDiv);
-    var expected;
-    if (testcase.expected_output)
-    {
-      expected = testcase.expected_output;
-    }
-    else
-    {
-      expected = "Hidden Result";
-    }
-    renderData_ignoreID(expected, expTestValDiv);
-
-    this._addFaceColumnToTestRow($newRow, testcase.passed_test);
-    //this._addDebugColumnToTestRow($newRow, testcase.debug);
-    this._addA11yToTestRow($newRow,
+      this._addFaceColumnToTestRow($newRow, testcase.passed_test);
+      //this._addDebugColumnToTestRow($newRow, testcase.debug);
+      this._addA11yToTestRow($newRow,
       this._accessibilityOutput(testcase.test_val),
       testcase.passed_test,
-      this._accessibilityOutput(testcase.expected_output));
-
+      this._accessibilityOutput(expected));
+    }
     return $newRow;
 
   }
